9ea8df45578eb50ff7477533375131c987e6444f
Francois Roewer-Despres <francois-rd@users.noreply.github.com>
2022-02-05 01:01:53 -0500

    Tweaked prelab in preparation for Lab 1.

--- labs/prelab/main.py
+++ labs/prelab/main.py
@@ -21,7 +21,7 @@ print(W[-5:])
 # Import words from RG65 Table 1.
 with open("../../data/RG65-Table1-words.txt") as f:
     rg65_words = [w.strip() for w in f.readlines()]
-# Add RG65 to W.
+# Add to W all words RG65 that are not already in W.
 W = list(set(W) | set(rg65_words))
 
 # Step 3:
@@ -29,7 +29,13 @@ from scipy.sparse import lil_matrix
 
 # Create a sparse word-context matrix.
 M1 = lil_matrix((len(W), len(W)))
-# Compute bigrams for each sentence individually.
+# Compute bigrams for each sentence individually. This is different from
+# computing the bigrams over all the words, since that would be including
+# bigrams from two sentences that are not actually consecutive. That is to
+# say, using "brown.words()" means that you get all sentences from all Brown
+# Corpus documents collapsed into a single sequence, so you would be finding
+# bigrams that join the last sentence of document 1 with the first sentence
+# of document 2 and so on.
 bigrams = [list(nltk.bigrams(w.lower() for w in s)) for s in brown.sents()]
 # Compute bigram frequencies over all sentences.
 bigram_freqs = nltk.FreqDist([b for sb in bigrams for b in sb]).most_common()
@@ -49,12 +55,12 @@ import math
 # Convert the sparse matrix to a more efficient format.
 M1_coo = M1.tocoo()
 # Since the bigram frequencies are computed differently than the unigram
-# frequencies (see the try/except of Step 3), we can't use unigram counts in
-# the PPMI calculations. Instead, we recompute unigram frequencies from row
-# and column sums of the word-context matrix.
+# frequencies (see comments in Step 3), we can't use unigram counts from Step 1
+# in these PPMI calculations. Instead, we recompute unigram frequencies from row
+# and column sums of M1.
 M1_row_sums, M1_col_sums = M1_coo.sum(axis=1).transpose(), M1_coo.sum(axis=0)
 M1_row_sums, M1_col_sums = M1_row_sums.tolist()[0], M1_col_sums.tolist()[0]
-# Same argument for the total sum.
+# Same argument for recomputing the total sum.
 M1_sum = M1_coo.sum()
 # Create a sparse PPMI matrix.
 M1_plus = lil_matrix((len(W), len(W)))
@@ -67,7 +73,11 @@ for r, c, f in zip(M1_coo.row, M1_coo.col, M1_coo.data):
 from sklearn.decomposition import TruncatedSVD
 
 # Apply TruncatedSVD to perform LSA on a PPMI matrix. Do this 3 times for each
-# of 10, 100, and 300 dimensions.
+# of 10, 100, and 300 dimensions. The reason for using TruncatedSVD instead of
+# PCA is that PCA cannot operate on sparse matrices:
+#   https://stackoverflow.com/questions/33603787/performing-pca-on-large-sparse-matrix-by-using-sklearn
+# In addition to this point, the documentation of TruncatedSVD explicitly states
+# that TruncatedSVD operating on word-context matrices is a way to perform LSA.
 M2_10 = TruncatedSVD(n_components=10)
 M2_10.fit(M1_plus)
 M2_100 = TruncatedSVD(n_components=100)
@@ -77,7 +87,8 @@ M2_300.fit(M1_plus)
 
 # Step 6:
 
-# By Step 1, all words in RG65 are in W, so we just load Table 1 of RG65.
+# By Step 1, all words in RG65 are in W already, or were added to it, so we just
+# load the entirety of Table 1 of RG65.
 with open("../../data/RG65-Table1.csv") as f:
     rg65_table1 = [line.strip().split(',') for line in f.readlines()]
 P = [(w1, w2) for w1, w2, _ in rg65_table1]
@@ -87,8 +98,10 @@ S = [float(row[2]) for row in rg65_table1]
 from sklearn.metrics.pairwise import cosine_similarity as cosim
 
 P_indices = [(W_indices[w1], W_indices[w2]) for w1, w2 in P]
+
 def similarity_from_map(map_func):
     return [cosim(map_func(i), map_func(j))[0][0] for i, j in P_indices]
+
 S_M1 = similarity_from_map(lambda i: M1[i])
 S_M1_plus = similarity_from_map(lambda i: M1_plus[i])
 S_M2_10 = similarity_from_map(lambda i: M2_10.transform(M1_plus[i]))
